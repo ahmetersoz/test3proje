1b6a74d45a50943d907d8fc486d9eb553e2a218a
ahmet <ahmetersz@gmail.com>
2023-01-30 21:53:27 +0300

    proje start

---
+++ src/deneme.java
@@ -0,0 +1,9 @@
+public class deneme {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <5 ; i++) {
+            System.out.println("şampiyon cimbom");
+
+        }
+    }
+}
